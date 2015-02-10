d84d6be206cb58ed67e01145b7eaf13dd485e9f0
fnkomo@myseneca.ca <Faith@Faiths-MacBook-Air.local>
2015-02-10 10:16:16 -0500

    first commit

---
+++ Swirl_L1_Basic-Building-Blocks.R
@@ -0,0 +1,45 @@
+install.packages("swirl")
+library("swirl")
+ls()
+swirl()
+5+7
+x <- 5 +7
+x
+y <- x - 3
+y
+c(1.1, 9, 3.14)
+bye()
+swirl()
+play()
+ls
+ls()
+nxt()
+bye
+bye()
+swirl()
+c(1.1, 9, 3.14)
+c(1.1, 9, 3.14)
+info()
+nxt()
+c(1.1, 9, 3.14).
+c(1.1, 9, 3.14)
+c(1.1, 9, 3.14)
+z <- c(1.1, 9, 3.14)
+c?
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+sqrt(z - 1)
+my_sqrt <- sqrt(z - 1)
+my_sqrt
+my_div <- (z / my_sqrt)
+my_div <- z / my_sqrt
+my_div
+c(1,2,3,4) + c(0,10)
+c(1,2,3,4) + c(0,10, 100)
+z * 2 + 1000
+my_div
+swirl()
+getwd()
+exit
